d5381b5096dfbfd19c03baca614f36400ad37964
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-01-05 16:59:08 +0000

    added history test

--- spec.js
+++ spec.js
@@ -4,6 +4,13 @@ describe('angularjs homepage', function() {
   var secondNumber = element(by.model('second'));
   var goButton = element(by.id('gobutton'));
   var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a,b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
 
   beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
@@ -27,5 +34,14 @@ describe('angularjs homepage', function() {
     secondNumber.sendKeys(6);
     goButton.click();
     expect(latestResult.getText()).toEqual('10');
+  });
+
+  it('should have a history', function() {
+    add(1, 2);
+    add(3, 4);
+    expect(history.count()).toEqual(2);
+    add(5 ,6);
+    expect(history.count()).toEqual(3);
+
   });
 });
